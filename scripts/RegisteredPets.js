d5b1b3a105d1dd4066be93b6dedeade7195c00e2
dparsley1203 <dparsley1203@gmail.com>
2021-06-08 15:16:59 -0500

    added click event

--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,12 +1,30 @@
 import { getPets } from "./database.js"
+import { getWalkers} from "./database.js"
 
 const pets = getPets()
+const walkers = getWalkers()
+
+document.addEventListener("click",
+    (event) => {
+        if (event.target.id.startsWith("pet")) {
+
+            const [, petId] = event.target.id.split("--")
+
+            const clickedPet = pets.find( (pet) => pet.id === parseInt(petId))
+            const findWalker = walkers.find( (walker) => walker.id === clickedPet.walkerId)
+            
+            window.alert(`${clickedPet.name} is being walked by ${findWalker.name}`)
+        }
+        
+
+    }
+)
 
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li id="pet--${pet.id}">${pet.name}</li>`
     }
 
     petHTML += "</ul>"
